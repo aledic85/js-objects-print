9f1eeb9c1484ca253cc53ab450e8c184163be04d
aledic85 <dicuollo85@gmail.com>
2019-02-22 14:45:39 +0100

    completed

--- student_objects.js
+++ student_objects.js
@@ -1,7 +1,7 @@
 function student() {
   var stud = {nome: "Mario", cognome: "Rossi", età: 17}
   for (var field in stud) {
-    document.getElementById("primo_task").innerHTML += "Primo task: " + stud[field] + "<br>"
+    document.getElementById("primo_task").innerHTML += "Primo task: " + stud[field] + "<br>";
   }
 }
 
@@ -9,19 +9,19 @@ function nameSurnameStamp(arr) {
   for (var i = 0; i < arr.length; i++) {
     var n = arr[i].nome;
     var c = arr[i].cognome;
-    document.getElementById("secondo_task").innerHTML +=  "Secondo task: " + n + c + "<br>"
+    document.getElementById("secondo_task").innerHTML +=  "Secondo task: " + n + c + "<br>";
   }
 }
 
-function addStudent (arr) {
+function addStudent(arr) {
   do {
   var newStud = {nome:"", cognome:"", età:0};
   arr.push(newStud);
   newStud.nome = prompt("Dimmi il nome dello studente: ");
   newStud.cognome = prompt("Dimmi il cognome dello studente: ");
-  newStud.età = prompt("Dimmi l'età dello studente: ")
+  newStud.età = prompt("Dimmi l'età dello studente: ");
 } while (prompt("Vuoi aggiungere un altro studente") != "no");
-  document.getElementById("terzo_task").innerHTML +=  "Terzo task: " + JSON.stringify(arr) + "<br>"
+  document.getElementById("terzo_task").innerHTML +=  "Terzo task: " + JSON.stringify(arr) + "<br>";
 }
 
 var studs = [
@@ -29,7 +29,7 @@ var studs = [
   {nome: "Marco", cognome: "Verdi", età: 18},
   {nome: "Andrea", cognome: "Gialli", età: 18},
   {nome: "Alessandro", cognome: "Blu", età: 18},
-  {nome: "Francesco", cognome: "Marroni", età: 18},
+  {nome: "Francesco", cognome: "Marroni", età: 18}
 ];
 
 student();
